cf4814759d77a15f891aa574ccb06a0c731bb457
Didiel Figueroa <didiel.f@gmail.com>
2021-05-16 16:55:14 -0400

    Updates

---
+++ precache-manifest.40b5b5469c1cc47e2cdd2be37cde2f3a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b050d832a156755e96688b638578ab4",
+    "url": "/heroes-villians-app/index.html"
+  },
+  {
+    "revision": "8be744aad700eefd768d",
+    "url": "/heroes-villians-app/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "1f87616a4150bf86a3f6",
+    "url": "/heroes-villians-app/static/css/main.dbd07cce.chunk.css"
+  },
+  {
+    "revision": "8be744aad700eefd768d",
+    "url": "/heroes-villians-app/static/js/2.cbbd0a81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/heroes-villians-app/static/js/2.cbbd0a81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f87616a4150bf86a3f6",
+    "url": "/heroes-villians-app/static/js/main.f3c09ee1.chunk.js"
+  },
+  {
+    "revision": "05046babded629c5fdf5",
+    "url": "/heroes-villians-app/static/js/runtime-main.f5ecda8d.js"
+  },
+  {
+    "revision": "2c00c32e6c843b37e3e158477661ba88",
+    "url": "/heroes-villians-app/static/media/BG_blue_comic.2c00c32e.png"
+  },
+  {
+    "revision": "cd513abb61169e9f74a6bb42e02cef43",
+    "url": "/heroes-villians-app/static/media/BG_red_comic.cd513abb.png"
+  },
+  {
+    "revision": "13a5cc68335aa169eb0d894e2088c85f",
+    "url": "/heroes-villians-app/static/media/logo_hv.13a5cc68.png"
+  },
+  {
+    "revision": "cfab40fe1950b3fd9da65120f6ec01a8",
+    "url": "/heroes-villians-app/static/media/red_blue_BG.cfab40fe.png"
+  }
+]);
